628b04c410b6b2fc087b10d7218df2fa8372ff5e
GiAndrelee <157076843+GiAndrelee@users.noreply.github.com>
2025-12-14 10:42:07 -0500

    Add models/User.js to define User Sequelize model

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db/sequelize');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: 'athlete'
+  }
+});
+
+module.exports = User;
